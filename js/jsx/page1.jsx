3a970773a24b5ff519ae3eebd345a4f1d4666214
xuj <xuj@bjyn.com>
2017-11-30 17:05:37 +0800

    Signed-off-by: xuj <xuj@bjyn.com>

---
+++ js/jsx/page1.jsx
@@ -0,0 +1,9 @@
+define('',['react','reactdom'],function(React,ReactDOM){
+  var test = React.createClass({
+        render: function() {
+          return (
+            <div>哈哈哈哈哈!!!</div>
+          );
+        }
+      });
+});
